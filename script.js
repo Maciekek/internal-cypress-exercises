0c9ddbb47d1b07587e2778be6c03f9e9935ccea2
maciejkucharski <maciej.kucharski@senti1.com>
2021-07-15 08:47:21 +0200

    init

---
+++ script.js
@@ -0,0 +1,64 @@
+// Zadanie:
+// Wywołanie:
+// Output:
+
+// Zadanie: Napisz funkcje, która wyświetli na ekran Twoje imie (użyj zmiennej do wpisania w kodzie swojego imienia)
+// Wywołanie: sayMyName()
+// Output: "Maciek" - tu twoje imie :)
+
+
+// Zadanie: Napisz funkcje, która przyjmie Twoje imie w parametrze.
+// Wywołanie: sayMyName(yourNameVariable)
+// Output: "Maciek" - tu twoje imie :)
+
+
+// Zadanie. Napisz funkcje dodającą 2 liczby
+// Wywołanie: sum(1,2)
+// Output: 3
+
+
+// Zadanie. Napisz funkcję, która przyjmuje 3 parametry. 2 pierwsze z nich to liczby, na których ma zostać przeprowadzona operacja matematyczna.
+// Trzeci parametr to działanie jakie ma zostać przeprowadzone.
+// Wywołanie: calculate(1,2, "+")
+// Output: 3
+//
+// Wywołanie: calculate(10, 5, "-")
+// Output: 5
+
+
+// Zadanie: Masz taką tablice
+const cars = ['bmw', 'mercedes', 'skoda', 'toyota', 'renault', 'ferrari']
+// Wyświetl z osobna każdy samochów w takim formacie: "Kupisz u nas samochód marki: xxx", za xxx ma być podstawiony każdy element z tablicy
+
+// Output:
+// Kupisz u nas samochód marki: bmw
+// Kupisz u nas samochód marki: mercedes
+// ...
+
+// Zrób to na dwa sposoby, wykorzystaj for i .map
+
+
+// Zadanie: Masz taką tablice:
+const people = [{name: "Jan", age: 20}, {name: "Monika", age: 45}, {name: "Kasia", age: 13}, {name: "Michal", age: 10}, {name: "Jacek", age: 50}]
+// napisz funkcje, która będzie przyjmowała tablicę w parametrze, i będzie zwracała tylko osoby pełnoletnie
+
+// Wywołanie: getAdultPerson(people)
+// Output: [{name: "Kasia", age: 13}, {name: "Michal", age: 10}]
+
+
+
+// Zadanie: Pracuj na tablicy z poprzedniego zadania:
+// napisz funkcje, która będzie przyjmowała tablicę w parametrze, i będzie zwracała Imiona osób pełnoletnich
+
+// Wywołanie: getAdultPersonName(people)
+// Output: ['Kasia', 'Michal']
+
+
+
+
+
+
+
+
+
+
